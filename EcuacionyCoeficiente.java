95e13446329b8c1afe661cbf2e2a5563aa41f4dd
AlexZama-San <61293885+AlexZama-San@users.noreply.github.com>
2020-10-27 16:07:50 -0600

    cambios y avance

--- EcuacionyCoeficiente.java
+++ EcuacionyCoeficiente.java
@@ -1,35 +1,47 @@
+import java.util.Vector;
 
 public class EcuacionyCoeficiente {
 
     public static void EcuacionLineal(){
         Metodos xD = new Metodos();
         //obtencion de vectores
-    int[] x={100,90,80,45,50,50,60,40,25,20};
-    int[] xc={100,90,80,45,50,50,60,40,25,20};
-    int[] xcc={100,90,80,45,50,50,60,40,25,20};
-    int[] y={3,5,9,10,20,21,24,24,27,35};
-    int[] x2=xD.Recorreyeleva(xc);
-    int[] xy=xD.Recorreymultiplica( xcc, y);
+    float[] x={100,90,80,45,50,50,60,40,25,20};
+    float[] xc={100,90,80,45,50,50,60,40,25,20};
+    float[] xcc={100,90,80,45,50,50,60,40,25,20};
+    float[] y={3,5,9,10,20,21,24,24,27,35};
+    float[] x2=xD.Recorreyeleva(xc);
+    float[] xy=xD.Recorreymultiplica( xcc, y);
 
     //Procedimiento de ecuacion (sumas y formulas)
-    int sumax=xD.Recorreysuma(x);
-    int sumay=xD.Recorreysuma(y);
-    int sumax2=xD.Recorreysuma(x2);
-    int sumaxy=xD.Recorreysuma(xy);
+    Vector<Float> sumas=new Vector<Float>(5,1);
+    sumas.add(xD.Recorreysuma(x));
+    sumas.add(xD.Recorreysuma(y));
+    sumas.add(xD.Recorreysuma(y));
+    sumas.add(xD.Recorreysuma(x2));
+    sumas.add(xD.Recorreysuma(xy));
     //sumas de formula a
-    float suma1=xy.length*(sumaxy);
-    float suma2=sumax*sumay;
-    float suma3=x2.length*(sumax2);
-    float suma4=(sumax)*sumax;
-    float a=(suma1-suma2)/(suma3-suma4);
+    Vector<Float> formulas=new Vector<Float>(4,1);
+    formulas.add(xy.length*(sumas.get(4)));
+    formulas.add(sumas.get(0)*sumas.get(1));
+    formulas.add(x2.length*(sumas.get(3)));
+    formulas.add((sumas.get(0))*sumas.get(0));
+    float a=(formulas.get(0)-formulas.get(1))/(formulas.get(2)-formulas.get(3));
     //sumas de formula b
-    float suma5=a*sumax;
-    float b=sumay-suma5;
+    float suma5=a*sumas.get(0);
+    float b=sumas.get(1)-suma5;
     b=b/x.length;
     System.out.println("y="+a+"x+"+b);
     
     }
     public static void CoeficienteLineal(){
         Metodos xD = new Metodos();
+        float[] x={100,90,80,45,50,50,60,40,25,20};
+        float[] xc={100,90,80,45,50,50,60,40,25,20};
+        float[] xcc={100,90,80,45,50,50,60,40,25,20};
+        float[] y={3,5,9,10,20,21,24,24,27,35};
+        
+    }
+    public static void main(String []args) {
+        EcuacionLineal();
     }
 }
--- Metodos.java
+++ Metodos.java
@@ -1,25 +1,25 @@
 public class Metodos {
-    public int Recorreysuma(int[] x){
-        int suma=0;
+    public float Recorreysuma(float[] x){
+        float suma=0;
         for(int recorre=0;recorre<x.length;recorre++){
             suma=suma+x[recorre];
         }
         return suma;
     }
-    public int[] Recorreyeleva(int[] xcc){
+    public float[] Recorreyeleva(float[] xcc){
         for(int recorre=0;recorre<xcc.length;recorre++){
             xcc[recorre]=xcc[recorre]*xcc[recorre];
         }
         return xcc;
     }
-    public int[] Recorreymultiplica(int[] zc,int[] y){
-        int[] xy=zc;
+    public float[] Recorreymultiplica(float[] zc,float[] y){
+        float[] xy=zc;
         for(int recorre=0;recorre<zc.length;recorre++){
             xy[recorre]=zc[recorre]*y[recorre];
         }
         return zc;
     }
-    public float RegladeTres(float x,int y){
+    public float RegladeTres(float x,float y){
         float result=(y*100)/x;
         return result;
     }
