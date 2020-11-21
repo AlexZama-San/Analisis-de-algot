a428b6928af96dd7e49d616349ac242677306e8e
AlexZama-San <61293885+AlexZama-San@users.noreply.github.com>
2020-11-21 13:58:48 -0600

    FizzPescao

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,18 +1,16 @@
 public class FizzBuzz {
     public static void main(String arg[]){
 
-        String fiz=" Fizz";
-        String buz=" Buzz";
 
-        for (int c=0;c<=100;c++){
+        for (int c=1;c<=100;c++){
             if (c%3==0 && c%5==0){
-                System.out.println(c+""+fiz+""+buz);
+                System.out.println(c+" Fizz Buzz");
             }else{
                 if(c%3==0){
-                    System.out.println(c+""+fiz);
+                    System.out.println(c+" Fizz");
                 }else{
                     if(c%5==0){
-                        System.out.println(c+""+buz);
+                        System.out.println(c+" Buzz");
                     }else{
                         System.out.println(c);
                     }
